05c395a0a98847adba852caefaaaa3894b9660cf
Julian Palacci <julian@ubuntu>
2019-06-15 14:08:44 -0300

    Add orf analysis to ex5

--- src/ex5.py
+++ src/ex5.py
@@ -1,7 +1,8 @@
 import sys
 import os
-if len (sys.argv) != 3:
-    print("Invalid params: 1) In file path - 2) Out file path")
+from Bio import SeqIO
+if len (sys.argv) != 5:
+    print("Invalid params: 1) In file path - 2) Out file for domain analysis 3) Nucleotide gbk 4) Out file for ORF" )
     sys.exit(1)
 
 def getmotifs(file_content):
@@ -16,7 +17,14 @@ def getmotifs(file_content):
         os.system("patmatmotifs temp_fasta.fasta " + sys.argv[2])
         os.remove('temp_fasta.fasta')
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print("Unexpected error:", sys.exc_info()[0])
         raise
 
+def getorf():
+    SeqIO.convert(sys.argv[3], "genbank", "temp.fasta", "fasta")
+    os.system("getorf temp.fasta " + sys.argv[4])
+    os.remove("temp.fasta")
+
+
 getmotifs(open(sys.argv[1]))
+getorf()
